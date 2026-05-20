61d56489afb84cb6b4360b39cd2e379a9b3a6a5c
Lyam Samuel Cardona <ly.samu19@gmail.com>
2026-05-20 21:19:33 +0000

    asa

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -7,10 +7,12 @@ from mangum import Mangum
 
 app = FastAPI()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
-app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+BASE_DIR = Path(__file__).resolve().parent
+
+
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
